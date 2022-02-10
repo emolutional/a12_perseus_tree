1290b46d5dc332d274ea94aec667219df9363314
emolutional <99024555+emolutional@users.noreply.github.com>
2022-02-10 10:07:41 +0800

    Update AndroidProducts.mk
    modify aosp2evolution
     based on lolipuru's device tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_perseus.mk
+    $(LOCAL_DIR)/evolution_perseus.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_perseus-user \
-    aosp_perseus-userdebug \
-    aosp_perseus-eng
+    evolution_perseus-user \
+    evolution_perseus-userdebug \
+    evolution_perseus-eng
